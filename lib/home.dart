b5019805db25a872b13b085c8bd7b04fa10521fa
Kenenna cc <kenennacc@gmail.com>
2021-07-03 19:37:27 +0100

    column rows

--- lib/home.dart
+++ lib/home.dart
@@ -12,25 +12,49 @@ class _HomeScreenState extends State<HomeScreen> {
   int _counter = 0;
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.grey.shade300,
-      appBar: AppBar(
-        leading: const Icon(
-          Icons.chevron_left,
-          size: 30,
-          color: Colors.white,
+        backgroundColor: Colors.grey.shade300,
+        appBar: AppBar(
+          leading: const Icon(
+            Icons.chevron_left,
+            size: 30,
+            color: Colors.white,
+          ),
+          elevation: 10,
+          backgroundColor: Colors.orangeAccent,
+          centerTitle: true,
+          actions: [Icon(Icons.shopping_bag_outlined)],
+          title: const Text(
+            "Home Screen",
+            style: TextStyle(
+                letterSpacing: 1,
+                color: Colors.white,
+                fontSize: 20,
+                fontWeight: FontWeight.normal),
+          ),
         ),
-        elevation: 10,
-        backgroundColor: Colors.orangeAccent,
-        centerTitle: true,
-        title: const Text(
-          "Home Screen",
-          style: TextStyle(
-              letterSpacing: 1,
-              color: Colors.white,
-              fontSize: 20,
-              fontWeight: FontWeight.normal),
-        ),
-      ),
-    );
+        body: Container(
+            /*color: Colors.pinkAccent,
+            height: 200, width: 200,*/
+            child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            Container(color: Colors.pinkAccent, width: 200, height: 200
+                /*const Text(
+              'Burger',
+              style: TextStyle(fontSize: 15, color: Colors.white),
+            ),*/
+                ),
+
+            Container(
+              color: Colors.greenAccent,
+              width: 200,
+              height: 200,
+            ) /*const Text(
+              'five dollars',
+              style: TextStyle(fontSize: 12, color: Colors.black),
+            ),*/
+            //Container(color: Colors.greenAccent, height: 200, width: 200),
+          ],
+        )));
   }
 }
